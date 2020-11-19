5225c3f24b53fdd1f4b268c60ddd24d312f2317f
Joseph Wong <blowfishpro@users.noreply.github.com>
2020-11-18 19:49:14 -0800

    wip rails 6

--- README.md
+++ README.md
@@ -17,86 +17,39 @@ git clone git@github.com:fiveplusone/turkopticon-rails-app.git
 cd turkopticon-rails-app
 ```
 
-1. ### Ruby 1.8.7
+1. ### Ruby 2.7.1
+
+#### Recommended
+
+Install [RVM](https://rvm.io/) (ruby version manager)
+
+Then install ruby 2.7.1
 
 ```bash
-gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-
-\curl -sSL https://get.rvm.io | bash
-
-rvm install ruby-1.8.7-p374 --with-openssl-lib=[openssl@1.0]/lib --with-openssl-include=[openssl@1.0]/include --with-openssl-dir=[openssl@1.0]
-
-rvm use ruby-1.8.7-p374
+rvm install 2.7.1
 ```
 
-Since this is an older version of ruby, [versions 1.1+ of openssl do not work with it](https://github.com/rvm/rvm/issues/4781). So you will need to install openssl version 1.0.x. On a mac, if you have [`brew`](https://brew.sh/), you can run `brew install rbenv/tap/openssl@1.0` and your directory will be `/usr/local/opt/openssl@1.0`. 
+#### Alternate
 
-If you have trouble with the 'gpg' instruction, refer to this [rvm page](https://rvm.io/rvm/security). 
+Install the correct version of ruby directly. Please see instructions on [the ruby website](https://www.ruby-lang.org/).
 
-To test ruby has been correctly installed, try installing `rails` with the following command:
+2. ### Gems
 
-```bash
-gem install rails -v 2.3.11
-```
+Gems are managed by bundler. Run `bundle install` to install the correct versions of all gems.
 
-
-
-2. ### MySQL
+3. ### MySQL
 
 The software we will be using requires version 5 of mysql. `brew install mysql@5.7` will install the appropriate version. For other operating systems, or details/alternatives, refer to [the official installation guide](https://dev.mysql.com/doc/refman/5.7/en/installing.html).
 
+The app expects that it can access mysql as the user `root` with no password in development mode. Please talk to us if you have an alternate setup that doesn't work with this assumption.
+
+Make sure the mysql server is running. If you installed mysql with homebrew it may be running already, but if not it can be started as `brew services start mysql@5.7`
+
+Run the following to configure the development database:
 ```bash
-# make sure mysql has been started 
-# command similar to mysql.server start or brew services start mysql@5.7
-# mysql -u root -p password (if you've set a password)
-$ mysql -u root 
-mysql> create database turkopticon_devel;
-mysql> create user 'turkopticon_dev'@'localhost' identified by 'PASSWORD';
-mysql> grant all privileges on turkopticon_devel.* to 'turkopticon_dev'@'localhost';
-# exit mysql with ctrl+d
-$ mysql -u root turkopticon_devel < ./db/turkopticon-empty-structure.sql
+bundle exec rake db:create db:migrate
 ```
 
-The password that you set for the host must be the same values that you enter into the `database.yml` file so you should save it.
+4. ### Starting the app
 
-Note: The `$` and `mysql>` are both command prompts, and should not be copies when entering the command.
-
-
-
-3. ### Gems
-
-```bash
-gem install fastthread -v 1.0.7
-gem install rails -v 2.3.11
-gem install mysql -v 2.9.1
-gem install json -v 1.8.1
-gem install haml -v 3.1.2
-gem install --local tmp/gem/mislav-will_paginate-2.3.11.gem --no-rdoc --no-ri # from turkoptica-rails-app dir
-```
-
-
-
-4. ### Local Env Files
-
-We now need to create a few config files that are specific to your dev environment.
-
-- environment.rb
-
-  ````bash
-  # make a copy of the example
-  cd config
-  cp environment-example.rb environment.rb
-  ````
-
-  Then open environment.rb and replace 'secret' with any number of size 30+.
-
-- database.yml
-
-  ```bash
-  # make a copy of the example
-  cp database-example.yml database.yml
-  ```
-
-  Then open `databse.yml` and on line 6, replace `'password'` with the password you chose in step 2 (MySQL).
-
-You should now be able to start the application. You can run this by going into the `script` directory and running the command `./server start`. Visit [localhost:3000](localhost:3000) to see the website!
+You should now be able to start the application. You can run this by running the command `bundle exec passenger start`. Visit [localhost:3000](localhost:3000) to see the website!
--- config/routes.rb
+++ config/routes.rb
@@ -1,52 +1,53 @@
-ActionController::Routing::Routes.draw do |map|
-  map.root :controller => "main", :action => "info"
-  map.connect '2016survey', :controller => "main", :action => "survey2016"
-  map.connect 'forum', :controller => "forum", :action => "index"
-  map.connect 'rules', :controller => "main", :action => "rules"
-  map.connect 'stats', :controller => "stats"
-  map.connect 'mod', :controller => "mod"
-  map.connect 'x', :controller => "main", :action => "x"
-  map.connect 'pri', :controller => "main", :action => "pri"
-  map.connect 'issues', :controller => "main", :action => "issues"
-  map.connect 'login', :controller => "reg", :action => "login"
-  map.connect 'logout', :controller => "reg", :action => "logout"
-  map.connect 'register', :controller => "reg", :action => "register"
-  map.connect 'report', :controller => "main", :action => "add_report"
-  map.connect 'edit/:id', :controller => "main", :action => "edit_report"
-  map.connect 'reports', :controller => "main", :action => "index"
-  map.connect 'edit/:id', :controller => "main", :action => "edit_report"
-  map.connect 'info', :controller => "main", :action => "info"
-  map.connect 'ditz', :controller => "main", :action => "ditz"
-  map.connect 'blog', :controller => "main", :action => "blog"
-  map.connect 'feed', :controller => "main", :action => "blogfeed"
-  map.connect 'help', :controller => "main", :action => "help"
-  map.connect 'aves/:id', :controller => "main", :action => "averages"
-  map.connect 'post/:id', :controller => "main", :action => "post"
-  map.connect 'flag/:id', :controller => "main", :action => "flag"
-  map.connect 'add_flag/:id', :controller => "main", :action => "add_flag"
-  map.connect 'requesters', :controller => "main", :action => "requesters"
-  map.connect 'all_requesters', :controller => "main", :action => "all_requesters"
-  map.connect 'requesters.csv', :controller => "main", :action => "requesters", :format => "csv"
-  map.connect 'settings', :controller => "reg", :action => "settings"
-  map.connect 'my_flagged', :controller => "main", :action => "my_flagged"
-  map.connect 'flagged', :controller => "main", :action => "flagged"
-  map.connect 'hidden', :controller => "main", :action => "hidden"
-  map.connect 'my_reviews', :controller => "main", :action => "my_reviews"
-  map.connect 'by/:id', :controller => "main", :action => "reports_by"
-  map.connect 'flagged_by/:id', :controller => "main", :action => "flagged_by"
-  map.connect 'comments_by/:id', :controller => "main", :action => "comments_by"
-  map.connect 'all_by/:id', :controller => "main", :action => "all_by"
-  map.connect 'reviews_by_one_page/:id', :controller => "main", :action => "reports_by_one_page"
-  map.connect 'admin', :controller => "admin", :action => "index"
-  map.connect 'wth', :controller => "main", :action => "wth"
-  map.connect ':id', :controller => "main", :action => "index"
-  map.connect 'get_report/:id', :controller => "main", :action => "report"
-  map.connect 'requester_stats/:id', :controller => "main", :action => "requester_stats"
-  map.connect 'attrs/:id', :controller => "main", :action => "requester_attrs"
-  map.connect 'attrsv2/:id', :controller => "main", :action => "requester_attrs_v2"
-  map.connect 'confirm/:hash', :controller => "reg", :action => "confirm"
-  map.connect 'enable_commenting/:id', :controller => "mod", :action => "enable_commenting"
-  map.connect 'disable_commenting/:id', :controller => "mod", :action => "disable_commenting"
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+Rails.application.routes.draw do
+  root :controller => "main", :action => "info"
+  match '2016survey', :controller => "main", :action => "survey2016", :via => :all
+  match 'forum', :controller => "forum", :action => "index", :via => :all
+  match 'rules', :controller => "main", :action => "rules", :via => :all
+  match 'stats', :controller => "stats", :via => :all
+  match 'mod', :controller => "mod", :via => :all
+  match 'x', :controller => "main", :action => "x", :via => :all
+  match 'pri', :controller => "main", :action => "pri", :via => :all
+  match 'issues', :controller => "main", :action => "issues", :via => :all
+  match 'login', :controller => "reg", :action => "login", :via => :all
+  match 'logout', :controller => "reg", :action => "logout", :via => :all
+  match 'register', :controller => "reg", :action => "register", :via => :all
+  match 'report', :controller => "main", :action => "add_report", :via => :all
+  match 'edit/:id', :controller => "main", :action => "edit_report", :via => :all
+  match 'reports', :controller => "main", :action => "index", :via => :all
+  match 'edit/:id', :controller => "main", :action => "edit_report", :via => :all
+  match 'info', :controller => "main", :action => "info", :via => :all
+  match 'ditz', :controller => "main", :action => "ditz", :via => :all
+  match 'blog', :controller => "main", :action => "blog", :via => :all
+  match 'feed', :controller => "main", :action => "blogfeed", :via => :all
+  match 'help', :controller => "main", :action => "help", :via => :all
+  match 'aves/:id', :controller => "main", :action => "averages", :via => :all
+  match 'post/:id', :controller => "main", :action => "post", :via => :all
+  match 'flag/:id', :controller => "main", :action => "flag", :via => :all
+  match 'add_flag/:id', :controller => "main", :action => "add_flag", :via => :all
+  match 'requesters', :controller => "main", :action => "requesters", :via => :all
+  match 'all_requesters', :controller => "main", :action => "all_requesters", :via => :all
+  match 'requesters.csv', :controller => "main", :action => "requesters", :format => "csv", :via => :all
+  match 'settings', :controller => "reg", :action => "settings", :via => :all
+  match 'my_flagged', :controller => "main", :action => "my_flagged", :via => :all
+  match 'flagged', :controller => "main", :action => "flagged", :via => :all
+  match 'hidden', :controller => "main", :action => "hidden", :via => :all
+  match 'my_reviews', :controller => "main", :action => "my_reviews", :via => :all
+  match 'by/:id', :controller => "main", :action => "reports_by", :via => :all
+  match 'flagged_by/:id', :controller => "main", :action => "flagged_by", :via => :all
+  match 'comments_by/:id', :controller => "main", :action => "comments_by", :via => :all
+  match 'all_by/:id', :controller => "main", :action => "all_by", :via => :all
+  match 'reviews_by_one_page/:id', :controller => "main", :action => "reports_by_one_page", :via => :all
+  match 'admin', :controller => "admin", :action => "index", :via => :all
+  match 'wth', :controller => "main", :action => "wth", :via => :all
+  match ':id', :controller => "main", :action => "index", :via => :all
+  match 'get_report/:id', :controller => "main", :action => "report", :via => :all
+  match 'requester_stats/:id', :controller => "main", :action => "requester_stats", :via => :all
+  match 'attrs/:id', :controller => "main", :action => "requester_attrs", :via => :all
+  match 'attrsv2/:id', :controller => "main", :action => "requester_attrs_v2", :via => :all
+  match 'confirm/:hash', :controller => "reg", :action => "confirm", :via => :all
+  match 'enable_commenting/:id', :controller => "mod", :action => "enable_commenting", :via => :all
+  match 'disable_commenting/:id', :controller => "mod", :action => "disable_commenting", :via => :all
+  match ':controller/:action/:id', :via => :all
+  match ':controller/:action/:id.:format', :via => :all
+  match ':controller/:action', :via => :all
 end
