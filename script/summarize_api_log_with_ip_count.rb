aea673feede06311644b8ce70ed73d8079f55cfa
Six Silberman <six@wtf.tw>
2020-12-13 11:41:41 -0800

    update summarize traffic ruby script

--- script/summarize_api_log_with_ip_count.rb
+++ script/summarize_api_log_with_ip_count.rb
@@ -6,6 +6,10 @@ tmpfile = dir + "multi-attrs.php.summary.tmp"
 `grep -E "#{datetime}:[[:digit:]]{2}" #{logfile} > #{tmpfile}`
 count = `wc -l #{tmpfile}`.split(" ").first
 ipcount = `grep -Eoh "[1-9][[:digit:]]{0,2}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}" #{tmpfile} | sort | uniq | wc -l`
+denylog = dir + "multi-attrs.php.denied.log"
+denytmp = dir + "multi-attrs.php.denied.tmp"
+`grep -E "#{datetime}:[[:digit:]]{2}" #{denylog} > #{denytmp}`
+denycount = `wc -l #{denytmp}`.split(" ").first
 bancount = `fail2ban-client status turkopticon-api | grep -E "Currently banned" | grep -Eo "[0-9]\+"`
 top_cpu = `top -b -n2 -d0.01 | grep Cpu | tail -n1` # see note 1
 cpu_use = top_cpu.split(" ")[1]
@@ -18,7 +22,7 @@ fail2ban_mem = top_fail2ban[9]
 top_mysqld = `top -b -n1 | grep mysqld | grep -v mysqld_safe`.split(" ")
 mysqld_cpu = top_mysqld[8]
 mysqld_mem = top_mysqld[9]
-`echo "[#{datetime}] #{count.strip} - #{ipcount.strip} IPs - #{bancount.strip} bans - CPU%: #{cpu_use.strip} - MEM USE #{mem_use_g.to_s} / #{mem_tot_g.to_s} GB - mysqld CPU%: #{mysqld_cpu.strip} MEM%: #{mysqld_mem.strip} - fail2ban-server CPU%: #{fail2ban_cpu.strip} MEM%: #{fail2ban_mem.strip}" >> #{outfile}`
+`echo "[#{datetime}] #{count.strip} - #{ipcount.strip} IPs - #{denycount} rejected - #{bancount.strip} bans - CPU%: #{cpu_use.strip} - MEM USE #{mem_use_g.to_s} / #{mem_tot_g.to_s} GB - mysqld CPU%: #{mysqld_cpu.strip} MEM%: #{mysqld_mem.strip} - f2bsrv CPU%: #{fail2ban_cpu.strip} MEM%: #{fail2ban_mem.strip}" >> #{outfile}`
 
 # note 1:
 # the line "should" be
